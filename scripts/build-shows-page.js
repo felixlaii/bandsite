66b4a16d3ffc7700dc103f55305480070ca591dc
felixlaii <felix.lai@hotmail.com>
2021-10-17 13:57:50 -0400

    added shows information with JS

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,33 +1,57 @@
 let showConcerts = [
     {
+        subdate: 'DATE',
         date: 'Mon Sept 06 2021',
+        subvenue: 'VENUE',
         venue: 'Ronald Lane',
+        sublocation: 'LOCATION',
         location: 'San Francisco, CA',
+        button: 'Buy Tickets',
     },
     {
+        subdate: 'DATE',
         date: 'Tues Sept 21 2021',
+        subvenue: 'VENUE',
         venue: 'Pier 3 East',
+        sublocation: 'LOCATION',
         location: 'San Francisco, CA',
+        button: 'Buy Tickets',
     },
     {
+        subdate: 'DATE',
         date: 'Fri Oct 15 2021',
+        subvenue: 'VENUE',
         venue: 'View Lounge',
+        sublocation: 'LOCATION',
         location: 'San Francisco, CA',
+        button: 'Buy Tickets',
     },
     {
+        subdate: 'DATE',
         date: 'Sat Nov 06 2021',
+        subvenue: 'VENUE',
         venue: 'Hyatt Agency',
+        sublocation: 'LOCATION',
         location: 'San Francisco, CA',
+        button: 'Buy Tickets',
     },
     {
+        subdate: 'DATE',
         date: 'Fri Nov 26 2021',
+        subvenue: 'VENUE',
         venue: 'Moscow Center',
+        sublocation: 'LOCATION',
         location: 'San Francisco, CA',
+        button: 'Buy Tickets',
     },
     {
+        subdate: 'DATE',
         date: 'Wed Dec 15 2021',
+        subvenue: 'VENUE',
         venue: 'Press Club',
+        sublocation: 'LOCATION',
         location: 'San Francisco, CA',
+        button: 'Buy Tickets',
     }
 ]
 
@@ -37,17 +61,33 @@ function displayShow (object) {
     div.classList.add ('shows')
     showsContainer.appendChild (div)
 
+    let subdate = document.createElement ('h1')
+    subdate.innerText = object.subdate
+    div.appendChild(subdate)
+
     let date = document.createElement ('h2')
     date.innerText = object.date
     div.appendChild(date)
+
+    let subvenue = document.createElement ('h1')
+    subvenue.innerText = object.subvenue
+    div.appendChild(subvenue)
 
     let venue = document.createElement('p')
     venue.innerText = object.venue
     div.appendChild(venue)
 
+    let sublocation = document.createElement ('h1')
+    sublocation.innerText = object.sublocation
+    div.appendChild(sublocation)
+
     let location = document.createElement ('p')
     location.innerText = object.location
     div.appendChild(location)
+
+    let button = document.createElement ('button')
+    button.innerText = object.button
+    div.appendChild(button)
 }
 
 window.onload = () => {
