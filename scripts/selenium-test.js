7ec24c76de5983b0954adb89e12641d37773a123
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-10-30 13:54:02 -0400

    testing

--- scripts/selenium-test.js
+++ scripts/selenium-test.js
@@ -10,21 +10,27 @@ async function runTest() {
     // launch chrome browser, method specifies that the test will run on chrome, await is used because build() returns a promise and await ensures that the program waits for the promise to resolve before proceeding.
     let driver = await new Builder(). forBrowser('chrome').setChromeOptions(chromeOptions).build();
 
+    // code inside this block is executed, and if any error occurs, they can be caught and handled in catch.
     try {
-        // navigate to your javascript page
+        // navigate to your javascript page, it waits until the page is fully loaded before proceeding to the next step
         await driver.get('/build-shows-page.js');
 
         // wait until an element is present (for example, wait for an element with ID 'showsContainer')
+        // driver.wait pauses the script execution until a specific condition is met. 
         await driver.wait(until.elementLocated(By.id('showsContainer')), 5000);
 
         // perform actions (click buttons, fill forms, etc.)
         // example: click a button with id 'buyButton'
+        // .click simulates a user click on the found element, triggering the associated JavaScript events.
         await driver.findElement(By.id('buyButton')).click();
 
         // add more actions and assertions as needed
 
+    // any errors that occurred during the try block are caught, and an error message is logged to the console.
     } catch (error) {
         console.error('test failed with error', error);
+    
+    // contains cleanup operations. 
     } finally {
         //close the browser
         await driver.quit()
